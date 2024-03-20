a4687525f1605e7bfbaa1f82dccfcf3da0a2d13e
Wonderful Carrot <wonderarry@gmail.com>
2024-03-20 16:31:25 +0300

    optimized the scrpt for reading large files

--- alembic/versions/45a475651931_add_golden_dataset_for_customer_.py
+++ alembic/versions/45a475651931_add_golden_dataset_for_customer_.py
@@ -20,7 +20,15 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     with open('sql/2_golden_prediction.up.sql', 'r', encoding='utf-8') as f:
-        op.execute(f.read())
+        sql_command = ''
+        for line in f:
+            # Accumulate lines until a semicolon is encountered
+            sql_command += line.strip()
+            if sql_command.endswith(';'):
+                # Execute the accumulated SQL command
+                op.execute(sql_command)
+                # Reset the accumulated SQL command
+                sql_command = ''
     with open('sql/3_golden_summary.up.sql', 'r', encoding='utf-8') as f:
         op.execute(f.read())
     pass
